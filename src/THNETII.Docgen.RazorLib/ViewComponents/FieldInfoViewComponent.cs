bbf984b3fdedd02657ac4689b75d7b12d9b4b865
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-10-20 16:57:31 +0200

    Stub: FieldInfo View Component

---
+++ src/THNETII.Docgen.RazorLib/ViewComponents/FieldInfoViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace THNETII.Docgen.RazorLib.ViewComponents
+{
+    public class FieldInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(FieldInfo fieldInfo) =>
+            View(fieldInfo);
+    }
+}
